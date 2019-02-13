95d0b96bef981a61778891de04e2eaba4674dd5e
Administrator <Administrator@QH-20170907BNDI>
2019-02-13 17:19:48 +0800

    日志

--- blockchain/src/main/java/test/Log4jTestDemo.java
+++ blockchain/src/main/java/test/Log4jTestDemo.java
@@ -1,15 +1,19 @@
 package test;
+import org.checkerframework.common.reflection.qual.GetClass;
+import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
 public class Log4jTestDemo {
-    private static Logger logger = LoggerFactory.getLogger(Log4jTestDemo.class);
+    private Logger logger = LoggerFactory.getLogger(getClass());
 
+    
     /**
      * @param args
      */
-    public static void main(String[] args) {
+    @Test
+    public void sss() {
     	logger.info("this is a example");
     	logger.info("Method:Hello");
         logger.info("World");
